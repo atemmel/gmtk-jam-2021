6e323476dfd1f002d2e8c589ed1058ffa4ded1f3
Alexander Norgren <alexandernorgren2@gmail.com>
2021-06-12 19:30:04 +0200

    Fattade problemet med targetPosition, och unmergeade våra bulletPrefabs

--- Assets/Scripts/Fox Scripts/enemy_behaviour.cs
+++ Assets/Scripts/Fox Scripts/enemy_behaviour.cs
@@ -4,56 +4,66 @@ using UnityEngine;
 
 public class enemy_behaviour : MonoBehaviour
 {
-
     public GameObject player;
     public GameObject bullet;
-    Vector3 enemy_velocity;
-    Vector3 acc;
+    //Vector3 enemy_velocity;
+    //Vector3 acc;
     public float startLerpingDistance;
 
     public int maximum_dist;
-    readonly int minimum_dist = 4;
+    //public int minimum_dist;
     public float speed;
     float timer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemy_velocity = getPositionDiff().normalized*speed;
+        //enemy_velocity = getPositionDiff().normalized*speed;
         timer = Time.realtimeSinceStartup;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos_diff = getPositionDiff();
-
-        //Bestämmer vilken hastighet objektet har just nu
-        var targetPosition = (transform.position - player.transform.position).normalized * maximum_dist;
+        //Vector3 pos_diff = getPositionDiff();
+        var targetPosition = player.transform.position + ((transform.position - player.transform.position).normalized * maximum_dist);
         var positionDelta = targetPosition - transform.position;
+        var newVelocity = positionDelta.normalized * speed;
 
-        /*if (positionDelta.magnitude < startLerpingDistance)
+        if (positionDelta.magnitude < startLerpingDistance)
         {
-            GetComponent<Rigidbody2D>().velocity = Vector3.Lerp(positionDelta.normalized, positionDelta.normalized * speed, positionDelta.magnitude / startLerpingDistance);
+            GetComponent<Rigidbody2D>().velocity = Vector3.Lerp(Vector3.zero, newVelocity, positionDelta.magnitude / startLerpingDistance);
         }
-        else*/
+        else
         {
-            GetComponent<Rigidbody2D>().velocity = positionDelta.normalized * speed;
+            GetComponent<Rigidbody2D>().velocity = newVelocity;
         }
+
+        //var targetPosition = transform.position;
+        //if (getPositionDiff().magnitude > minimum_dist)
+        //{
+        //    targetPosition = player.transform.position + ((transform.position - player.transform.position).normalized * minimum_dist);
+        //}
+        //else if (getPositionDiff().magnitude < maximum_dist)
+        //{
+        //    targetPosition = player.transform.position + ((transform.position - player.transform.position).normalized * maximum_dist);
+        //}
 
         //Kulor som skickas
         if (Time.realtimeSinceStartup - timer > 2)
         {
             var bull = Instantiate(bullet, transform.position, transform.rotation);
-            var velocity = pos_diff.normalized;
+            var velocity = getPositionDiff().normalized;
             bull.GetComponent<Rigidbody2D>().velocity = velocity;
             timer = Time.realtimeSinceStartup;
         }
     }
+
     Vector3 getPositionDiff()
     {
         return player.transform.position - transform.position;
     }
+
     float getAngle()
     {
         return Vector3.Angle(transform.position, player.transform.position);
