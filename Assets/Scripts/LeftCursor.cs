c7cc5d3e8dcfeab8196d0c8a72fb3680fd9a74b0
atemmel <temmeladam@gmail.com>
2021-06-12 12:26:18 +0200

    Added script to yank the cargo

--- Assets/Scripts/LeftCursor.cs
+++ Assets/Scripts/LeftCursor.cs
@@ -4,32 +4,26 @@ using UnityEngine;
 
 public class LeftCursor : MonoBehaviour
 {
+	Renderer _renderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		//var translation = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f) * 0.02f;
-		//transform.position = transform.position + translation;
+		var translation = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0.0f) * 0.030f;
+		transform.position = transform.position + translation;
 
-		if (Input.GetKey(KeyCode.W)) {  
-			transform.position = transform.position + new Vector3(0.0f, 1.0f, 0.0f) * 0.02f;
-		}  
+ 		Vector3 lowerCameraBounds = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f, 0));
+ 		Vector3 upperCameraBounds = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight, 0));
 
-		if (Input.GetKey(KeyCode.S)) {  
-			transform.position = transform.position + new Vector3(0.0f, -1.0f, 0.0f) * 0.02f;
-		}  
-
-		if (Input.GetKey(KeyCode.A)) {  
-			transform.position = transform.position + new Vector3(-1.0f, 0.0f, 0.0f) * 0.02f;
-		}  
-
-		if (Input.GetKey(KeyCode.D)) {  
-			transform.position = transform.position + new Vector3(1.0f, 0.0f, 0.0f) * 0.02f;
-		}  
+		if(transform.position.x < lowerCameraBounds.x || transform.position.y < lowerCameraBounds.y 
+				|| transform.position.x > upperCameraBounds.x || transform.position.y > upperCameraBounds.y) {
+			transform.position = transform.position - translation;
+		}
     }
 }
