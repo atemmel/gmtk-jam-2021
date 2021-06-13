3a9be70c054ba29fdec28de39d37c88fb0f347e1
Dustfoxx <fox.hamren@gmail.com>
2021-06-13 16:19:49 +0200

    Fixed so that the ships gets disabled

--- Assets/Scripts/Fox Scripts/collision_logic.cs
+++ Assets/Scripts/Fox Scripts/collision_logic.cs
@@ -21,6 +21,7 @@ public class collision_logic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
