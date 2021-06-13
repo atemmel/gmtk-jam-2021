91339bc775023cedf1a6e91aa277c519952568a1
Alexander Norgren <alexandernorgren2@gmail.com>
2021-06-13 14:10:42 +0200

    fixade score stuff och lite ui

--- Assets/Scripts/Fox Scripts/collision_logic.cs
+++ Assets/Scripts/Fox Scripts/collision_logic.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class collision_logic : MonoBehaviour
 {
-	public GameObject explosionVfx;
+    public GameObject explosionVfx;
     public int hp;
+
+    public GameObject score_empty;
+    public int hitScore;
+    public int killScore;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,16 +27,31 @@ public class collision_logic : MonoBehaviour
         if (collision.gameObject.layer == 6) //playerBullet layer
         {
             hp -= 1;
+            AddScore(hitScore);
         }
         else if (collision.relativeVelocity.magnitude > 20)
         {
             hp -= 3;
         }
 
-        if (hp <= 0) {
-			var effect = Instantiate(explosionVfx, transform.position, Quaternion.identity);
+        if (hp <= 0)
+        {
+            var effect = Instantiate(explosionVfx, transform.position, Quaternion.identity);
+            AddScore(killScore);
             Destroy(effect, 0.5f);
             Destroy(gameObject);
-		}
+        }
     }
+
+    private void AddScore(int score)
+    {
+        var down_bound = Camera.main.ScreenToWorldPoint(Vector3.zero).y;
+        var upper_bound = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, Camera.main.pixelHeight, 0.0f)).y;
+        if (transform.position.y > down_bound && transform.position.y < upper_bound)
+        {
+            var add_score = Instantiate(score_empty, transform.position, Quaternion.identity);
+            add_score.GetComponent<ScoreAdder>().value = score;
+        }
+    }
+
 }
