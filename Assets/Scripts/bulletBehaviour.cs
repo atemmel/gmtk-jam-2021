c243013a676179a70b8d125b753cbdf4b2b58f1f
atemmel <temmeladam@gmail.com>
2021-06-13 02:16:59 +0200

    Player bullets now despawn

--- Assets/Scripts/bulletBehaviour.cs
+++ Assets/Scripts/bulletBehaviour.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class bulletBehaviour : MonoBehaviour
 {
     Rigidbody2D _rigidbody2D;
+
+	Renderer _renderer;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _rigidbody2D.velocity = Vector2.up * 5;
+		_renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+		if(!_renderer.isVisible) {
+			Destroy(gameObject);
+		}
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
